77943f572b2d9953d3124cb678553d6a11a97c56
raj-j-shah <rajcoder19@gmail.com>
2024-01-14 13:59:20 +0530

    Bugs fixed

--- SERVER/index.js
+++ SERVER/index.js
@@ -5,19 +5,8 @@ const jwt = require('jsonwebtoken');
 const sg = require('@sendgrid/mail')
 dotenv.config();
 const pwd = process.env.PASSWORD;
-sg.setApiKey(process.env.APIKEY)
-async function esend(){
-await sg.send({
-  from: 'rshah213203@gmail.com',
-  to: 'raj21032003@gmail.com' ,
-  subject: 'SyncWrite Edit Document Invitation',
-  text: `Click this : http://localhost:3000/documents/7ca31f4e-9c8b-4643-9d6a-8539658bce4e \n to edit the SyncWrite Document.\n Happy Editing!\n Team SyncWrite`
-})
-.then((v)=>{
-  console.log(v+"email send!");
-})
-}
-esend();
+
+
 
 mongoose.connect("mongodb+srv://raj:"+pwd+"@cluster0.wtliayy.mongodb.net/",{
     connectTimeoutMS: 30000, // Adjust timeout value as needed
